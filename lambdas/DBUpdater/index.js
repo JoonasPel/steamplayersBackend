800a42b2c4a320cdb3f96e7fcecb70c37845cf2c
Joonas Pelttari <joonas.pelttari98@gmail.com>
2023-09-26 19:59:21 +0300

    v2

--- lambdas/DBUpdater/index.js
+++ lambdas/DBUpdater/index.js
@@ -1,68 +1,74 @@
 const axios = require('axios');
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
-// setup
-const client = new Client({
+const pool = new Pool({
   host: "XXXX.eu-north-1.rds.amazonaws.com",
-  port: 'XXXX',
+  port: "XXXX",
   database: "XXXX",
   user: "XXXX",
   password: "XXXX",
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
-// connect outside handler to reuse con
-client.connect()
-  .then(() => { console.log('Connected to RDS'); })
-  .catch((error) => { console.error('Error connecting to RDS:', error); });
-
+const url = "https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=";
 exports.handler = async (event, context) => {
+  const client = await pool.connect();
+  
+  const unixTimestamp = Math.floor(Date.now() / 1000);
   const priority_table = "a_priority_table";
+  let rejectedSteamRequestsCounter = 0; 
+  let invalidPlayerCountOrIdCounter = 0;
+  
   try {
     const res = await client.query("SELECT gameid FROM "+priority_table+" WHERE priority=1");
     const ids = res?.rows.map(obj => obj?.gameid);
     if (ids?.length === 0) { throw new Error("no values found from "+priority_table); }
     
-    const unixTimestamp = Math.floor(Date.now() / 1000);
+    //const prom2 =  client.query("CREATE TABLE IF NOT EXISTS "+tableName+" (timestamp INT NOT NULL, playercount INT NOT NULL, PRIMARY KEY (timestamp))");
+    const results = await Promise.allSettled(ids.map(id => axios.get(url+id, {timeout: 15000}) ));
     
+    const queryPromises = [];
     let updateQuery = "UPDATE currentplayers SET playercount = CASE gameid";
-    for (let id of ids) {
-      let tableName = "gameid_" + id;
-      const prom1 = axios.get('https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid='+id, {timeout: 1000});
-      //const prom2 =  client.query("CREATE TABLE IF NOT EXISTS "+tableName+" (timestamp INT NOT NULL, playercount INT NOT NULL, PRIMARY KEY (timestamp))");
-      //const results = await Promise.allSettled([prom1, prom2]);
-      const results = await Promise.allSettled([prom1]);
-      const response = results[0]?.value;
-   
-      const currPlayers = response?.data?.response?.player_count;
-      if (!isNaN(currPlayers)) {
-        await client.query("INSERT INTO "+tableName+" VALUES ($1, $2)", [unixTimestamp, currPlayers]);
-        updateQuery += " WHEN "+id+" THEN "+currPlayers; 
+    results.map((result, index) => {
+      const id = ids[index];
+      if (result?.status === "fulfilled") {
+        const currPlayers = result?.value?.data?.response?.player_count;
+        if (!isNaN(currPlayers) && !isNaN(id)) {
+          queryPromises.push(client.query("INSERT INTO gameid_"+id+" VALUES ($1, $2)", [unixTimestamp, currPlayers]));
+          updateQuery += " WHEN "+id+" THEN "+currPlayers;
+        } else {
+          invalidPlayerCountOrIdCounter++;
+        }
+      } else {
+        rejectedSteamRequestsCounter++;
       }
-    }
-    
+    });
+    await Promise.allSettled(queryPromises);
+
     // update current to currentplayers table
     updateQuery += " ELSE playercount END";
     await client.query(updateQuery);
     
     // update 24h peak and bottom to currentplayers table
     const unix24hAgo = unixTimestamp - 86420;  // 20s "wiggle" room
-    let update_peak_query = "UPDATE currentplayers SET peak = CASE gameid"
-    let update_bottom_query = "UPDATE currentplayers SET bottom = CASE gameid"
+    let update_peak_query = "UPDATE currentplayers SET peak = CASE gameid";
+    let update_bottom_query = "UPDATE currentplayers SET bottom = CASE gameid";
     for (let id of ids) {
-      const res = await client.query("SELECT * FROM gameid_"+id+" WHERE timestamp>"+unix24hAgo+" ORDER BY playercount DESC LIMIT 300");
-      const peak = res.rows[0].playercount;
-      const bottom = res.rows[res.rowCount-1].playercount;
-      update_peak_query += " WHEN "+id+" THEN "+peak;
-      update_bottom_query += " WHEN "+id+" THEN "+bottom;
+      const res = await client.query("SELECT MAX(playercount), MIN(playercount) FROM gameid_"+id+" WHERE timestamp>"+unix24hAgo);
+      update_peak_query += " WHEN "+id+" THEN "+res?.rows[0]?.max;
+      update_bottom_query += " WHEN "+id+" THEN "+res?.rows[0]?.min;
     }
     update_peak_query += " ELSE peak END";
     update_bottom_query += " ELSE bottom END";
-    
     await client.query(`${update_peak_query}; ${update_bottom_query}`);
-
-    return { statusCode: 200, body: 'Data retrieved and saved to RDS successfully' };
+    
+    client.release();
+    return { statusCode: 200, body: {rejectedSteamRequestsCounter, invalidPlayerCountOrIdCounter} };
     
   } catch (error) {
     console.error('Error:', error);
-    return { statusCode: 500, body: 'An error occurred' };
+    client.release();
+    return { statusCode: 500, body: {rejectedSteamRequestsCounter, invalidPlayerCountOrIdCounter} };
   }
 };
