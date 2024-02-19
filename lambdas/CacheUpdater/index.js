24e62af757e77c632eb31428e5ed69b5f8c605d6
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-02-19 19:15:25 +0200

    refactor + env vars

--- lambdas/CacheUpdater/index.js
+++ lambdas/CacheUpdater/index.js
@@ -1,11 +1,17 @@
 const redis = require("redis");
 const { Client } = require("pg");
 
+const priorityTable = "a_priority_table";
+const currentTable = "currentplayers";
+const trendingTable = "a_trending_daily";
+const affDataTable = "affdata";
+const trendingGamesIncluded = 10;
+
 // connect to redis and test connection.
 // return client or undefined if error
-const setupAndTestConn = async () => {
+const connectRedis = async () => {
     const client = redis.createClient({
-        url: "redis://XXXX.eun1.cache.amazonaws.com:6379",
+        url: process.env.redisUrl,
     });
     try {
         await client.connect();
@@ -18,15 +24,15 @@ const setupAndTestConn = async () => {
     return undefined;
 };
 
-// connect to RDS(postgres)
+// connect to database
 // return client or undefined if error
 const connectRDS = async () => {
   const client = new Client({
-    host: "XXXX.eu-north-1.rds.amazonaws.com",
-    port: 'XXXX',
-    database: "XXXX",
-    user: "XXXX",
-    password: "XXXX",
+    host: process.env.hostUrl,
+    port: process.env.port,
+    database: process.env.databaseName,
+    user: process.env.user,
+    password: process.env.password,
   });
   try {
     await client.connect();
@@ -43,13 +49,14 @@ const closeConnections = async (redisClient, rdsClient) => {
     await rdsClient.end();
 };
 
-// ERROR HANDLING MISSING!!!!!!!!
+// TODO ERROR HANDLING
 // gets players information from currentplayers table
 const getStatisticsFromRDS = async (rdsClient) => {
     const response = await rdsClient.query(`
-        SELECT cp.gameid, cp.playercount, cp.peak, cp.bottom, COALESCE(pt.gamename, 'unknown') as gamename
-        FROM currentplayers as cp
-        LEFT JOIN a_priority_table as pt ON cp.gameid = pt.gameid
+        SELECT cp.gameid, cp.playercount, cp.peak, cp.bottom,
+        COALESCE(pt.gamename, 'unknown') as gamename
+        FROM ${currentTable} as cp
+        LEFT JOIN ${priorityTable} as pt ON cp.gameid = pt.gameid
         ORDER BY cp.playercount DESC;
     `);
     const rows = response.rows;
@@ -66,28 +73,24 @@ const getStatisticsFromRDS = async (rdsClient) => {
 
 const getDailyTrendingRDS = async (rdsClient) => {
     const response = await rdsClient.query(`
-        SELECT td.gameid, td.increase, COALESCE(pt.gamename, 'unknown') as gamename
-        FROM a_trending_daily as td
-        LEFT JOIN a_priority_table as pt ON td.gameid = pt.gameid
+        SELECT td.gameid, td.increase,
+        COALESCE(pt.gamename, 'unknown') as gamename
+        FROM ${trendingTable} as td
+        LEFT JOIN ${priorityTable} as pt ON td.gameid = pt.gameid
         ORDER BY td.increase DESC
-        LIMIT 10;
+        LIMIT ${trendingGamesIncluded};
     `);
     const rows = response.rows;
     return rows;
-    const items = [];
-    for (let row of rows) {
-        items.push({score: Number(row.playercount), value: JSON.stringify(row)});
-    }
-    return items;
 };
 
 const getAffDataRDS = async (rdsClient) => {
   const response = await rdsClient.query(`
     SELECT ad.gameid, ad.url, ad.price, ad.retailprice
-    FROM affdata as ad
-    LEFT JOIN a_trending_daily as td ON ad.gameid = td.gameid
+    FROM ${affDataTable} as ad
+    LEFT JOIN ${trendingTable} as td ON ad.gameid = td.gameid
     ORDER BY td.increase DESC
-    LIMIT 10;
+    LIMIT ${trendingGamesIncluded};
   `);
   const rows = response.rows;
   return rows;
@@ -100,36 +103,22 @@ const saveToRedis = async (redisClient, items) => {
     await redisClient.ZADD("players", items);
 };
 
-const divideItemsToChunks = (items, chunkSize) => {
-    const chunks = [];
-    for (let i = 0; i < items.length; i += chunkSize) {
-      const chunk = items.slice(i, i + chunkSize);
-      chunks.push(chunk);
-    }
-    return chunks;
-};
-
 exports.handler = async (event) => {
   try {
-    const redisClient = await setupAndTestConn();
+    const redisClient = await connectRedis();
     const rdsClient = await connectRDS();
     if (!redisClient) { throw new Error('redis connection error') }
     if (!rdsClient) { throw new Error('rds connection error') }
     
+    // get needed data from rds and save to redis
     const [items, items2] = await getStatisticsFromRDS(rdsClient);
     await saveToRedis(redisClient, items);
     // saves every game also as key(gamename)-value(data) for search feature
     await redisClient.MSET(items2);
-    
-    
-    // get TOP 10 daily trending from rds and save to redis
     const daily = await getDailyTrendingRDS(rdsClient);
     await redisClient.set('trendingDaily', JSON.stringify(daily));
-    
-    // get aff data for trending games from rds and save to redis.
     const affData = await getAffDataRDS(rdsClient);
     await redisClient.set('affData', JSON.stringify(affData));
-    
     
     await closeConnections(redisClient, rdsClient);
     return {statusCode: 200};
